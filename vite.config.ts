4682f92205e8f8e5622c44de612e136db15f5cb6
DanieleNebbiai <danielenebbiai10@gmail.com>
2024-10-05 18:25:25 +0200

    plspl

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ShadcnUI-main/",
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
